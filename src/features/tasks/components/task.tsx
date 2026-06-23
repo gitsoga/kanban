36d596d7ba845377dbf7f6374c4aa76acf2be527
gitsoga <sogayukie@gmail.com>
2026-06-23 12:08:16 +0900

    表示修正

--- src/features/tasks/components/task.tsx
+++ src/features/tasks/components/task.tsx
@@ -15,9 +15,9 @@ type TaskProps = {
 
 export function Task({ task }: TaskProps) {
   return (
-    <Link to={`/tasks/${task.id}`}>
+    <Link to={`/tasks/${task.id}`} className="col-span-2 block">
       <div
-        className={`col-span-2 m-2 ${task.priority == "高" ? "bg-red-200" : task.priority == "中" ? "bg-yellow-200" : "bg-gray-200"}`}
+        className={`m-2 ${task.priority == "高" ? "bg-red-200" : task.priority == "中" ? "bg-yellow-200" : "bg-gray-200"}`}
       >
         <div>{task.title}</div>
         <div>{task.priority}</div>
